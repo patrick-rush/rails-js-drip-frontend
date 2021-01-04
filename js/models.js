0f2e0a45222d0791803ac15a270c2df2da2d2cf3
patrick-rush <patrick.a.rush@gmail.com>
2021-01-04 10:21:34 -0500

    added error handling

--- js/models.js
+++ js/models.js
@@ -13,7 +13,6 @@ class Plant {
     }
 
     static all() {
-        // find someway to prevent this from populating sidebar multiple times
         return fetch("http://localhost:3000/plants", {
             headers: {
                 "Accept": "application/json",
@@ -35,62 +34,6 @@ class Plant {
             })
     }
 
-/*
-                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <a href='#' class="text-sm font-medium text-gray-500">
-                    Plant 1
-                  </a>
-                </div>
-*/ 
-
-/*
-            <div id="rightContainerHeader" class="px-4 py-5 sm:px-6">
-              <h3 class="text-lg leading-6 font-medium text-gray-900">
-                Name of Plant
-              </h3>
-              <p class="mt-1 max-w-2xl text-sm text-gray-500">
-                Species of plant
-              </p>
-            </div>
-
-            Plant.rightContainer().querySelector(".")
-*/
-/*
-              <form action="#" method="POST">
-                <div class="shadow overflow-hidden sm:rounded-md">
-                    <div class="px-4 py-5 bg-white sm:p-6">
-                        <div class="grid grid-cols-6 gap-6">
-                            <div class="col-span-6 sm:col-span-3">
-                                <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
-                                <input type="text" name="name" id="name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                            </div>
-                            <div class="col-span-6 sm:col-span-3">
-                                <label for="species" class="block text-sm font-medium text-gray-700">Species</label>
-                                <input type="text" name="species" id="species" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                            </div>
-                            <div class="col-span-6 sm:col-span-4">
-                                <label for="location" class="block text-sm font-medium text-gray-700">Location</label>
-                                <input type="text" name="location" id="location" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                            </div>
-                            <div class="col-span-6 sm:col-span-3">
-                              <label for="fertalizing_frequency" class="block text-sm font-medium text-gray-700">Fertalizing Frequency</label>
-                              <input type="text" name="fertalizing_frequency" id="fertalizing_frequency" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                            </div>
-                            <div class="col-span-6 sm:col-span-3">
-                                <label for="watering_frequency" class="block text-sm font-medium text-gray-700">Watering Frequency</label>
-                                <input type="text" name="watering_frequency" id="watering_frequency" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                      <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Save
-                      </button>
-                    </div>
-                </div>
-              </form>
-
-*/
     static new() {
         console.log("got to new plant");
         let header = this.rightContainer().querySelector(".header");
@@ -129,72 +72,13 @@ class Plant {
                 this.rightContainer().querySelector(".location").textContent = plant.location;
                 this.rightContainer().querySelector(".watering_frequency").textContent = plant.watering_frequency;
                 this.leftContainer().append(plant.render())
-                // this.rightContainer().querySelector(".fertilizing_frequency").textContent = plant.fertilizing_frequency;
-                // this.rightContainer().appendChild(plant.renderPlant());
+                new FlashMessage({type: 'success', message: 'New plant added successfully'})
+            })
+            .catch(error => {
+                new FlashMessage({type: 'error', message: error});
             })
     }
 
-/*
-          <div id="rightContainer" class="bg-white sm:min-h-screen col-span-2 rounded-md shadow">
-            <div class="header px-4 py-5 sm:px-6">
-              <h3 class="title text-lg leading-6 font-medium text-gray-900">
-                Name of Plant
-              </h3>
-            </div>
-            <div class="border-t border-gray-200">
-              <div class="body">
-              <ul>
-                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <li class="text-sm font-medium text-gray-500">
-                    Location
-                  </li>
-                  <li class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-1">
-                    <a href="#" class="py-4 col-span-10 sm:col-span-4">Kitchen</a>
-                  </li>
-                  <li class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-1">
-                    <a href="#" class="my-4 text-right"><i class="fa fa-pencil-alt content-end"></i></a>      
-                  </li>
-                </div>
-                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <li class="text-sm font-medium text-gray-500">
-                    Watering Frequency
-                  </li>
-                  <li class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-1">
-                    <a href="#" class="py-4 col-span-10 sm:col-span-4">5 days</a>
-                  </li>
-                  <li class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-1">
-                    <a href="#" class="my-4 text-right"><i class="fa fa-minus"></i></a>
-                    <a href="#" class="my-4 text-right"><i class="fa fa-plus"></i></a>      
-                  </li>
-                </div>
-                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <li class="text-sm font-medium text-gray-500">
-                    Fertalizating Frequency
-                  </li>
-                  <li class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-1">
-                    <a href="#" class="py-4 col-span-10 sm:col-span-4">6 months</a>
-                  </li>
-                  <li class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-1">
-                    <a href="#" class="my-4 text-right"><i class="fa fa-minus"></i></a>
-                    <a href="#" class="my-4 text-right"><i class="fa fa-plus"></i></a>      
-                  </li>
-
-                </div>
-                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <li class="text-sm font-medium text-gray-500">
-                    Notes
-                  </li>
-                </div>  
-                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <li class="text-sm font-medium text-gray-500">
-                    Completed Care Events
-                  </li>
-                </div>
-
-              </ul>
-            </div>
-
-*/
 
     renderPlant() {
         // START HERE - MAKE THIS RENDER A PLANT'S INFO AND ALSO TOGGLE HIDDEN ON FORM 
@@ -229,9 +113,34 @@ class Plant {
         return this.element
     }
 }
+
 class CareEvent {
     constructor(attributes) {
         let whitelist = ["id", "event_type", "due_date", "completed", "plant_id"];
         whitelist.forEach(attr => this[attr] = attributes[attr]);
+    }
+}
+
+class FlashMessage {
+    constructor({type, message}) {
+        this.error = type === "error";
+        this.message = message;
+        this.render();
+    }
+    
+    container() {
+        return this.c ||= document.querySelector('#flash');
+    }
+
+    render() {
+        this.container().textContent = this.message;
+        this.toggleDisplay();
+        setTimeout(() => this.toggleDisplay(), 5000); 
+    }
+
+    toggleDisplay() {
+        console.log("we got to toggle display")
+        this.container().classList.toggle('opacity-0');
+        this.container().classList.toggle(this.error ? 'bg-red-200' : 'bg-gray-200');
     }
 }
